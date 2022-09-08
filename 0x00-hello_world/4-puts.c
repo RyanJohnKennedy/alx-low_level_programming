4cfa9ff6b73ecdffce4ef47cca1fecb641d6c085
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-08 12:03:03 -0700

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,8 @@
 /**
  *
  *  * main - Entry point
- *
- *   *
- *
+ *   
  *    * Description: prints "Programming is like building a multilingual puzzle"
- *
- *     *
  *
  *      * Return: Always 0 (Success)
  *
