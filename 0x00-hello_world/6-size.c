9842165f3f33f9ed58b40e4c27703407c54b8ac5
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-08 12:37:49 -0700

    fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - print the size of various types
  *
  * Description: prints the size of the various types
  *
  * Return: Always 0 (Seccess)
- *
  */
 
 int main(void)
