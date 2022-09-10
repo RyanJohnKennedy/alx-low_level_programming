57ec5d6199bdc31476264f8707f502e2b1089fb2
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-10 10:22:41 -0700

    step 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print letters reverse
+ *
+ * Description: print letters lowercase and backwards
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
