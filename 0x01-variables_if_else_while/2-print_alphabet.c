5767fd7ecd10a3b1ef0c0be855b5f706a0bf30bd
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-10 09:58:32 -0700

    step 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase letters
+ *
+ * Description: print all lower case letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putcher(i);
+		i++;
+	}
+	putcher(10);
+
+	return (0);
+}
