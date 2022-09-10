85ebe37d7ac516d12571ca065d8eded5049e3459
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-10 10:14:15 -0700

    step 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print digit numbers
+ *
+ * Description: print digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
