03bc30eff3c33b3469ddcf78090cbb6dcd005abb
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-10 10:03:32 -0700

    step 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters
+ *
+ * Description: print lowercase and upper letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
