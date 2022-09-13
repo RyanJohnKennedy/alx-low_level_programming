db2c3fd33fad89e937a3df5321fb9fafc9b6c9e6
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-13 15:40:20 -0700

    step 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @num1: integer
+ * @num2: integer
+ *
+ * Return: sum;
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
