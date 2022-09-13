2146ab6551a0b1e8cde632e72b620efd71b3ba78
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-13 14:28:50 -0700

    step 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an int
+ *
+ * Return: 1 (if letter)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
