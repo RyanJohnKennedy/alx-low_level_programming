fe0b65c0a3db7b1e8536574d8327e2c31af59ea0
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-13 15:36:21 -0700

    fix 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,15 @@
 
 void times_table(void)
 {
-	int row, column, product;
+	int row, column, product, ten, one;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
+			ten = product / 10;
+			one = product % 10;
 
 			if (column == 0)
 			{
@@ -25,13 +27,14 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(product + '0');
+				_putchar(one + '0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(product + '0');
+				_putchar(ten + '0');
+				_putchar(one + '0');
 			}
 		}
 		_putchar('\n');
