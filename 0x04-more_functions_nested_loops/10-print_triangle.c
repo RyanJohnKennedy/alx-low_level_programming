b1421b868345428e4f10212304bbd6f0d1a2f232
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-15 14:44:22 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: integer
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i = 0;
+	int s, a;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			for (s = size - 1; s > i; s--)
+				_putchar(' ');
+			for (a = 0; a < i + 1; a++)
+				_putchar('a');
+
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
