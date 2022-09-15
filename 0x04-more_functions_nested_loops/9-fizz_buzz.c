20a7787f2cce23365556017fe79f0f07e7b39dc7
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-15 14:34:11 -0700

    step 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fizz-buzz
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+				print("Buzz");
+			else
+				printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
