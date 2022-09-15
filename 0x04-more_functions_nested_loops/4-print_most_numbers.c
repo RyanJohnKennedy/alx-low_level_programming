8e5d9a75f95b885aabf122d40acc8a97f45aaf04
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-15 13:56:27 -0700

    step 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers but 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; c++)
+	{
+		if (a != '2' && c != '4')
+			_putchar(a);
+	}
+	_putchar('\n');
+}
