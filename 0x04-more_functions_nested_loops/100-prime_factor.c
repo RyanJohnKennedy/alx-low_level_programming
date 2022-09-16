a4892c08e5aa07dcfc2777b3d5630d56c5bc0245
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-16 14:00:42 -0700

    step 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the largest prime
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int prime = 612852475143;
+	long int i = prime;
+	long int d = 2;
+
+	while (i != d)
+	{
+		if (i % d == 0)
+			i = i / d;
+		else
+			d++;
+	}
+
+	printf("%d\n", i);
+
+	return (0);
+}
