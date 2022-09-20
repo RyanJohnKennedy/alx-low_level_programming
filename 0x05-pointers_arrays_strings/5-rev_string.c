d65a3802736a7e35a05a30ad5a7a60c7d1b9fbdf
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-20 12:29:10 -0700

    step 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char *temp = *s;
+	int index1 = 0;
+	int index2 = 0;
+
+	while (*temp[index1] != '0')
+		index1++;
+	index1--;
+	index2 = index1;
+	while (index1 >= 0)
+	{
+		*s[index2] = *temp[index1];
+		index1--;
+		index2++;
+	}
+}
