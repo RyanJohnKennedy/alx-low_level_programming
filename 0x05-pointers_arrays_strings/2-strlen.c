e4131fb8932ebb484c15da9159653100f9a8166a
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-20 12:05:01 -0700

    step 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of string
+ * @s: pointer
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (Length);
+}
