97e3358c3d7b94ebed6d74032a6a1f6dd4009d1e
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-20 13:06:02 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,8 @@ void puts_half(char *str)
 	while (str[index] != '\0')
 		index++;
 
-	if (index % 2 == 0)
-		half = index / 2;
-	else
-		half = (index - 1) / 2;
-
 	max = index;
-	index = half;
+	index = max / 2;
 
 	while (index <= max)
 	{
