0c2a8007193f80cf0d4c022d934c696c5e15eaaf
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-21 13:04:48 -0700

    step 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @a: string
+ * Return: string
+ */
+
+char *cap_string(char *a)
+{
+	int index = 0;
+
+	while (a[index])
+	{
+		index++;
+
+		while (!(a[index] >= 'a' && a[index] <= 'z'))
+			index++;
+
+		if (a[index - 1] == ' ' ||
+				a[index - 1] == '\t' ||
+				a[index - 1] == '\n' ||
+				a[index - 1] == ',' ||
+				a[index - 1] == ';' ||
+				a[index - 1] == '.' ||
+				a[index - 1] == '!' ||
+				a[index - 1] == '?' ||
+				a[index - 1] == '"' ||
+				a[index - 1] == '(' ||
+				a[index - 1] == ')' ||
+				a[index - 1] == '{' ||
+				a[index - 1] == '}')
+			a[index] -= 32;
+	}
+
+	return (a);
+}
