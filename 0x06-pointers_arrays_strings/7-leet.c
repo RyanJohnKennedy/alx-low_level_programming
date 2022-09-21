f13074af7abb72b37e8ac8e6b4d74990c37e221d
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-21 13:25:08 -0700

    fix 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,26 @@
 
 /**
  * leet - encodes a string into 1337
- * @a: string
+ * @src: string
  * Return: string
  */
 
-char *leet(char *a)
+char *leet(char *src)
 {
 	int i = 0;
 	int j = 0;
 
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (a[++i])
+	while (src[++i])
 	{
 		for (j = 0; j <= 7; j++)
 		{
-			if (a[i] == leet[j] ||
-					a[i] - 32 == leet[j])
-				a[i] = j + '0';
+			if (src[i] == leet[j] ||
+					src[i] - 32 == leet[j])
+				src[i] = j + '0';
 		}
 	}
 
-	return (a);
+	return (src);
 }
