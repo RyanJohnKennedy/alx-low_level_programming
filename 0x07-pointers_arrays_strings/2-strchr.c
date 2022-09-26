51e905e7b16d439b7191c9c9cda63fd4c65fb4cb
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-26 09:35:14 -0700

    step 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string
+ * @s: string pointer
+ * @c: charater
+ * Return: character pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	if (c == '\0')
+		return (s + i);
+
+	return (NULL);
+}
