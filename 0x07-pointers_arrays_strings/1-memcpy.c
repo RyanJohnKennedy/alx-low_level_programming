c41abd76255a26d1959b9dc40d2b9578c4993066
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-26 09:21:40 -0700

    fix 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (unsigned int i = 0; i < n; i++)
+	unsigned int i = 0;
+
+	for ( ; i < n; i++)
 		dest[i] = src[i];
 
 	return (dest);
