311687411e1d006790e436745b3b0dc082532ceb
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-30 12:09:59 -0700

    step 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument number
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int _attribute_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
