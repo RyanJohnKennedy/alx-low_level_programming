75640d8cd560d45da128a79212800a5a1b7d193e
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-09-30 12:16:09 -0700

    step 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: integer
+ * @argv: string array
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d/n", argc - 1);
+
+	return (0);
+}
