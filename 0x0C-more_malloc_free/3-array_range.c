7ac742aad5247aa842121b30deeeb542fdb2bd22
Ryan Kennedy <Ryan.Kennedy.John@gmail.com>
2022-10-06 13:02:05 -0700

    fix 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	memory = malloc(sizeof(*block) * ((max - min) + 1));
+	memory = malloc(sizeof(*memory) * ((max - min) + 1));
 	if (memory != NULL)
 	{
 		for (i = min; i <= max; i++)
